9b33b723c6365787e46f192ea73fe5d0b124985d
SeongMin Han <hsm950825@naver.com>
2019-10-27 19:25:29 +0900

    DRF 로그인/로그아웃 urls 추가

--- DjangoAPI/urls.py
+++ DjangoAPI/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from mystorage import urls
+from rest_framework import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mystorage.urls'))
+    path('', include('mystorage.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
