a17ab91e6f1116c527f3cd68b371a72f5762b9f1
SeongMin Han <hsm950825@naver.com>
2019-10-27 20:15:59 +0900

    File업로드 오버라이딩 추가

--- mystorage/views.py
+++ mystorage/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets
 from .models import Essay, Album, Files
 from .serializers import EssaySerializer, AlbumSerializer, FilesSerializer
 from rest_framework.filters import SearchFilter
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -36,3 +39,16 @@ class ImgViewSet(viewsets.ModelViewSet):
 class FileViewSet(viewsets.ModelViewSet):
     queryset = Files.objects.all()
     serializer_class = FilesSerializer
+
+    # 파일 업로드
+    parser_classes = (MultiPartParser, FormParser)
+
+    # create() => post()
+
+    def post(self, request, *args, **kwargs):
+        serializer = FilesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Reponse(serializer.error, status=HTTP_400_BAD_REQUEST)
