c8cae3718542572e90008e3cb3fe7fc2d9294475
SeongMin Han <hsm950825@naver.com>
2019-10-27 19:28:39 +0900

    SearchFilter & author 자동입력 추가

--- mystorage/views.py
+++ mystorage/views.py
@@ -1,7 +1,27 @@
 from rest_framework import viewsets
 from .models import Essay
 from .serializers import EssaySerializer
+from rest_framework.filters import SearchFilter
 
 class PostViewSet(viewsets.ModelViewSet):
-  queryset = Essay.objects.all()
-  serializer_class = EssaySerializer
+    queryset = Essay.objects.all()
+    serializer_class = EssaySerializer
+
+    # SearchFilter
+    filter_backends = [SearchFilter]
+    search_fields = ('title', 'body')
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    # 현재 request를 보낸 유저
+    # == self.request.user
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        if self.request.user.is_authenticated:
+            qs = qs.filter(author = self.request.user)
+        else : 
+            qs = qs.none()
+        return qs
